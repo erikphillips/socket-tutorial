cc291602b5789b2b7175d47c415d23f03d0b2b81
erikphillips <erik.phillips@icloud.com>
2017-03-08 16:28:37 -0800

    updates

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res){
 });
 
 var clients = 0;
+var roomno = 1;
 
 io.on('connection', function(socket) {
 	console.log("A user connected");
@@ -33,7 +34,22 @@ io.on('connection', function(socket) {
 	socket.on('clientEvent', function ( data ) {
 		console.log(data);
   	});
+
+	//Increase roomno 2 clients are present in a room.
+  	if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1)
+    	roomno++;
+  	socket.join("room-"+roomno);
+
+	//Send this event to everyone in the room.
+	io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
+
 });
+
+var nsp = io.of('/my-namespace');
+nsp.on('connection', function (socket) {
+	console.log('someone connected to namespace');
+	nsp.emit('hi', 'Hello, Everyone!');
+})
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
