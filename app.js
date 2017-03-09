66d2df60613b097dc560d9f2b7264633cb21d3e2
erikphillips <erik.phillips@icloud.com>
2017-03-08 16:08:47 -0800

    socket emitters

--- app.js
+++ app.js
@@ -6,12 +6,33 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+var clients = 0;
+
 io.on('connection', function(socket) {
 	console.log("A user connected");
 
+	clients++;
+
+	io.sockets.emit('broadcast', { description: clients + ' clients connected.' });
+
+	socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
+	socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
+
+	setTimeout(function () {
+		// This can be used to send a message via socket - not very useful
+		// socket.send('Sent a message 4 seconds after connection!');
+		// socket.emit('testerEvent', { description: 'A custom event named testerEvent!' });
+	}, 4000);
+
 	socket.on('disconnect', function () {
+		clients--;
+		io.sockets.emit('broadcast', { description: clients + ' clients connected.' });
 		console.log("A user disconnected");
 	});
+
+	socket.on('clientEvent', function ( data ) {
+		console.log(data);
+  	});
 });
 
 http.listen(3000, function(){
